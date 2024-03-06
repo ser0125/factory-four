9440dcccbd874856f01d9f505f4721020db8c20c
Sergio Llanos <sergio.llanos.garcia@gmail.com>
2024-03-06 17:29:24 -0500

    Add the Navbar and remove unused content

---
+++ components/core/Navbar.tsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <div className="flex bg-regal-blue py-5 text-white shadow-nav">
+          <h1>Logo</h1>
+          <h1>Status Bar</h1>
+        </div>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        'regal-blue': '#243c5a',
       },
-    },
-  },
+      boxShadow: {
+        'nav': '0px 2px 4px -1px rgba(0,0,0,0.2),0px 4px 5px 0px rgba(0,0,0,0.14),0px 1px 10px 0px rgba(0,0,0,0.12)'
+      }
+    }
+},
   plugins: [],
 };
 export default config;
