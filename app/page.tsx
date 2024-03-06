27b45b66d6c582d4d75ddeaf8bbdb36a722e14cc
Sergio Llanos <sergio.llanos.garcia@gmail.com>
2024-03-06 18:24:52 -0500

    Add the api url and set an interval to consume every 3 seconds to test

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
-'use client'
-import { useEffect } from "react";
+"use client";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Dashboard() {
+  const [apis, setApis] = useState({});
+
+  const fetchApi = async () => {
+    const url = "https://api.factoryfour.com/accounts/health/status";
+    try {
+      const { data } = await axios.get(url);
+      setApis(data);
+    } catch (e) {
+      console.log(e);
+      setApis({ success: false, message: e });
+    }
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetchApi();
+    }, 3000);
+    return () => clearInterval(interval);
+  }, []);
 
   return <div>Dashboard</div>;
 }
