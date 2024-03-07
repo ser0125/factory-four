ff71478b9b045f6bd452203a8fbac93ba04197ee
Sergio Llanos <sergio.llanos.garcia@gmail.com>
2024-03-06 19:23:04 -0500

    Create the CardContainer and add some improvements

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import axios from "axios";
+import CardContainer from "@/components/CardContainer";
+import { Apis } from "@/interfaces/Apis";
 
 const endpoints = [
   "accounts",
@@ -24,7 +26,7 @@ const endpoints = [
 ];
 
 export default function Dashboard() {
-  const [apis, setApis] = useState({});
+  const [apis, setApis] = useState<Apis>();
 
   const fetchApi = async (endpoint: string) => {
     const url = `https://api.factoryfour.com/${endpoint}/health/status`;
@@ -35,36 +37,33 @@ export default function Dashboard() {
         [endpoint]: data,
       }));
     } catch (e) {
-      console.log(e);
       setApis((previousState) => ({
         ...previousState,
-        [endpoint]: { success: false, message: 'Error' },
+        [endpoint]: { success: false, message: "Error" },
       }));
     }
   };
 
-  const getAllStatus = async () => {
+  const getAllStatus = useCallback(async () => {
     endpoints.forEach(async (endpoint) => await fetchApi(endpoint));
-  };
+  }, []);
 
   useEffect(() => {
     getAllStatus();
     const interval = setInterval(() => {
       getAllStatus();
-    }, 3000);
+    }, 15000);
     return () => clearInterval(interval);
-  }, []);
+  }, [getAllStatus]);
 
   return (
     <>
-      {endpoints.map((name, index) => {
-        return (
-          <>
-            <h1 key={name + index}>{name}</h1>
-            {apis && <h1>{apis[name]?.message}</h1>}
-          </>
-        );
-      })}
+      {apis &&
+        Object.keys(apis).map((name: string, index: number) => {
+          return (
+            <CardContainer key={name + index} name={name} data={apis[name]} />
+          );
+        })}
     </>
   );
 }
