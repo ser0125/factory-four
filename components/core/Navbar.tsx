dba819045a9bef9959c6dbddb31916e7f1e294c5
Sergio Llanos <sergio.llanos.garcia@gmail.com>
2024-03-07 15:38:30 -0500

    hot-fix add some images and details

--- components/core/Navbar.tsx
+++ components/core/Navbar.tsx
@@ -1,9 +1,18 @@
+import Image from "next/image";
+
 const Navbar = () => {
   return (
     <nav>
       <ul>
         <div className="flex bg-regal-blue py-5 text-white shadow-nav">
-          <h1>Logo</h1>
+          <Image
+            className="mx-2"
+            src="images/heart-pulse-solid.svg"
+            alt="heart"
+            width={0}
+            height={0}
+            style={{ width: 20, height: 20 }}
+          />
           <h1>Status Bar</h1>
         </div>
       </ul>
